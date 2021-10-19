423ee6f8486d3eeeb6211439dea4f631474aa8e9
jiyakeshwani <jiyakeshwani4@gmail.com>
2021-10-19 20:47:23 +0530

    block3

--- block-BJaadc/code/index.js
+++ block-BJaadc/code/index.js
@@ -1,17 +1,18 @@
 let message = 'If you are still thinking the answer is no!';
 
 // Log the length of the variable message like `The length of [message] is [length]`
+console.log("The lenght of ${message} is message.length")
 
 // Log the character at index 0 of the variable message like `The first character of [message] is [character]`
-
+console.log("The first character of message is message.charAt(0)");
 // Log the last character of message using length like `The last character of [message] is [character]`
-
+console.log("The last character of message is message.charAt()")
 // Log the index of word "you" in message
-
+console.log("message.indexOf('you')")
 // Log the index of word "still" in message
-
+console.log("message.indexOf('still')")
 // Log the index of word "answers" in message
-
+console.log("message.indexOf('answers')")
 // Log true or false based on whether the word "answers" exist in message or not
 
 // Log true or false based on whether the word "they" exist in message or not
